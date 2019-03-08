d93b336af3bb1b1a05ec75303645e47ad4f3b53b
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-03-08 22:39:25 +0100

    Add missing slice macros

--- src/array/slice.rs
+++ src/array/slice.rs
@@ -1,6 +1,10 @@
+#[macro_export]
 macro_rules! s {
-    ($start:expr, $step:expr, $end:expr) => ($start, $step, $end);
-    ($start:expr, $end:expr) => ($start, 1, $end);
-    (,$end:expr) => (0, 1, $end);
-    ($start:expr,) => ($start, 1, -1);
+    ($start:expr, $step:expr, $end:expr) => (($start, $step, $end));
+    ($start:expr, $end:expr) => (($start, 1, $end));
+    (,$end:expr) => ((0, 1, $end));
+    ($start:expr,) => (($start, 1, -1));
+    ($start:expr, $step:expr,) => (($start, $step, -1));
+    (,$step:expr, $end:expr) => ((0, $step, $end));
+    ($start:expr) => (($start, 1, $start + 1));
 }
