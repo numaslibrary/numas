e4d9f1d4b887eeaff3e6b9f98a713c1d543195bc
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-03-08 23:02:46 +0100

    Implement hyperbolic functions

--- src/mathematic/hyperbolic.rs
+++ src/mathematic/hyperbolic.rs
@@ -1,26 +1,38 @@
 use array::Array;
 
 
-fn sinh<T>(array: Array<T>) -> Array<T> {
-    return array;
+fn sinh<T>(array: Array<T>) -> Array<f64> {
+    let data = array.data.iter().map(|value| f64::sinh(value)).collect();
+
+    return Array::new(data, array.get_shape().clone());
 }
 
-fn cosh<T>(array: Array<T>) -> Array<T> {
-    return array;
+fn cosh<T>(array: Array<T>) -> Array<f64> {
+    let data = array.data.iter().map(|value| f64::cosh(value)).collect();
+
+    return Array::new(data, array.get_shape().clone());
 }
 
-fn tanh<T>(array: Array<T>) -> Array<T> {
-    return array;
+fn tanh<T>(array: Array<T>) -> Array<f64> {
+    let data = array.data.iter().map(|value| f64::tanh(value)).collect();
+
+    return Array::new(data, array.get_shape().clone());
 }
 
-fn arcsinh<T>(array: Array<T>) -> Array<T> {
-    return array;
+fn arcsinh<T>(array: Array<T>) -> Array<f64> {
+    let data = array.data.iter().map(|value| f64::asinh(value)).collect();
+
+    return Array::new(data, array.get_shape().clone());
 }
 
 fn arccosh<T>(array: Array<T>) -> Array<T> {
-    return array;
+    let data = array.data.iter().map(|value| f64::acosh(value)).collect();
+
+    return Array::new(data, array.get_shape().clone());
 }
 
 fn arctanh<T>(array: Array<T>) -> Array<T> {
-    return array;
+    let data = array.data.iter().map(|value| f64::atanh(value)).collect();
+
+    return Array::new(data, array.get_shape().clone());
 }
