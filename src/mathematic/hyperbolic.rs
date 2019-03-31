a909f22aafb052c84c8529404e02c0eb6aeb13ea
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-03-31 23:31:08 +0200

    Implement mathematic functions for struct instead

--- src/mathematic/hyperbolic.rs
+++ src/mathematic/hyperbolic.rs
@@ -1,74 +1,39 @@
 use array::Array;
 
+impl<T> Array<T> where T: Clone + Into<f64> {
+    /// Applies hyperbolic sine on elements from given array and creates new array
+    #[inline]
+    pub fn sinh(&self) -> Array<f64> {
+        return super::apply(&self, |value: &T| f64::sinh(value.clone().into()));
+    }
 
-/// Applies hyperbolic sine on elements from given array and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn sinh<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return super::apply(array, |value: &T| f64::sinh(value.clone().into()));
-}
+    /// Applies hyperbolic cosine on elements from given array and creates new array
+    #[inline]
+    pub fn cosh(&self) -> Array<f64> {
+        return super::apply(&self, |value: &T| f64::cosh(value.clone().into()));
+    }
 
-/// Applies hyperbolic cosine on elements from given array and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn cosh<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return super::apply(array, |value: &T| f64::cosh(value.clone().into()));
-}
+    /// Applies hyperbolic tangent on elements from given array and creates new array
+    #[inline]
+    pub fn tanh(&self) -> Array<f64> {
+        return super::apply(&self, |value: &T| f64::tanh(value.clone().into()));
+    }
 
-/// Applies hyperbolic tangent on elements from given array and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn tanh<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return super::apply(array, |value: &T| f64::tanh(value.clone().into()));
-}
+    /// Applies inverse hyperbolic sine on elements from given array and creates new array
+    #[inline]
+    pub fn arcsinh(&self) -> Array<f64> {
+        return super::apply(&self, |value: &T| f64::asinh(value.clone().into()));
+    }
 
-/// Applies inverse hyperbolic sine on elements from given array and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn arcsinh<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return super::apply(array, |value: &T| f64::asinh(value.clone().into()));
-}
+    /// Applies hyperbolic cosine on elements from given array and creates new array
+    #[inline]
+    pub fn arccosh(&self) -> Array<f64> {
+        return super::apply(&self, |value: &T| f64::acosh(value.clone().into()));
+    }
 
-/// Applies hyperbolic cosine on elements from given array and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn arccosh<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return super::apply(array, |value: &T| f64::acosh(value.clone().into()));
-}
-
-/// Applies hyperbolic tangent on elements from given array and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn arctanh<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return super::apply(array, |value: &T| f64::atanh(value.clone().into()));
+    /// Applies hyperbolic tangent on elements from given array and creates new array
+    #[inline]
+    pub fn arctanh(&self) -> Array<f64> {
+        return super::apply(&self, |value: &T| f64::atanh(value.clone().into()));
+    }
 }
--- src/mathematic/trigonometric.rs
+++ src/mathematic/trigonometric.rs
@@ -1,110 +1,57 @@
 use array::Array;
 
+impl<T> Array<T> where T: Clone + Into<f64> {
+    /// Applies sine on elements from given array and creates new array
+    #[inline]
+    pub fn sin(&self) -> Array<f64> {
+        return super::apply(&self, |value: &T| f64::sin(value.clone().into()));
+    }
 
-/// Applies sine on elements from given array and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn sin<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return super::apply(array, |value: &T| f64::sin(value.clone().into()));
-}
+    /// Applies cosine on elements from given array and creates new array
+    #[inline]
+    pub fn cos(&self) -> Array<f64> {
+        return super::apply(&self, |value: &T| f64::cos(value.clone().into()));
+    }
 
-/// Applies cosine on elements from given array and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn cos<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return super::apply(array, |value: &T| f64::cos(value.clone().into()));
-}
+    /// Applies inverse sine on elements from given array and creates new array
+    #[inline]
+    pub fn arcsin(&self) -> Array<f64> {
+        return super::apply(&self, |value: &T| f64::asin(value.clone().into()));
+    }
 
-/// Applies inverse sine on elements from given array and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn arcsin<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return super::apply(array, |value: &T| f64::asin(value.clone().into()));
-}
+    /// Applies inverse cosine on elements from given array and creates new array
+    #[inline]
+    pub fn arccos(&self) -> Array<f64> {
+        return super::apply(&self, |value: &T| f64::acos(value.clone().into()));
+    }
 
-/// Applies inverse cosine on elements from given array and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn arccos<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return super::apply(array, |value: &T| f64::acos(value.clone().into()));
-}
+    /// Applies inverse tangent on elements from given array and creates new array
+    #[inline]
+    pub fn arctan(&self) -> Array<f64> {
+        return super::apply(&self, |value: &T| f64::atan(value.clone().into()));
+    }
 
-/// Applies inverse tangent on elements from given array and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn arctan<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return super::apply(array, |value: &T| f64::atan(value.clone().into()));
-}
+    /// Converts elements from given array to degrees and creates new array
+    #[inline]
+    pub fn degrees(&self) -> Array<f64> {
+        return super::apply(&self, |value: &T| f64::to_degrees(value.clone().into()));
+    }
 
-/// Converts elements from given array to degrees and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn degrees<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return super::apply(array, |value: &T| f64::to_degrees(value.clone().into()));
-}
+    /// Converts elements from given array to radians and creates new array
+    #[inline]
+    pub fn radians(&self) -> Array<f64> {
+        return super::apply(&self, |value: &T| f64::to_radians(value.clone().into()));
+    }
 
-/// Converts elements from given array to radians and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn radians<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return super::apply(array, |value: &T| f64::to_radians(value.clone().into()));
-}
+    /// Converts elements from given array to degrees and creates new array
+    #[inline]
+    pub fn deg2rad(&self) -> Array<f64> {
+        return self.radians();
+    }
 
-/// Converts elements from given array to degrees and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn deg2rad<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return radians(array);
-}
-
-/// Converts elements from given array to radians and creates new array
-///
-/// # Arguments
-///
-/// * `array` - source array
-#[inline]
-pub fn rad2deg<T>(array: &Array<T>) -> Array<f64>
-    where T: Clone + Into<f64>
-{
-    return degrees(array);
+    /// Converts elements from given array to radians and creates new array
+    #[inline]
+    pub fn rad2deg(&self) -> Array<f64> {
+        return self.degrees();
+    }
 }
