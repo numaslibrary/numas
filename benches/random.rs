9117e8875bb6b06c5d9589dcb418a93e4471abcc
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-03-31 23:18:49 +0200

    Add benches

--- benches/random.rs
+++ benches/random.rs
@@ -9,7 +9,12 @@ mod tests {
 
 
     #[bench]
-    fn bench_add_two(b: &mut Bencher) {
+    fn random_fill(b: &mut Bencher) {
         b.iter(|| random::random::<f64>(vec![100,10,10,10,10]));
+    }
+
+    #[bench]
+    fn random_range(b: &mut Bencher) {
+        b.iter(|| random::random_range::<f64>(10.0, 20.0, vec![100,10,10,10,10]));
     }
 }
