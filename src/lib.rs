5efe63dd5d11c8178c1c8ad61e7650eef6542e15
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-08 23:13:38 +0200

    Add logo to docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![doc(html_logo_url = "https://raw.githubusercontent.com/numaslibrary/numaslibrary.github.io/master/assets/images/favicon.png", html_favicon_url = "https://raw.githubusercontent.com/numaslibrary/numaslibrary.github.io/master/assets/images/favicon.png")]
+
 pub mod array;
 pub mod shape;
 pub mod mathematic;
