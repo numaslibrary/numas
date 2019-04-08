732aaa21acba9b0c53062c1644dcf1e36067199d
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-08 22:06:15 +0200

    Reimplement slice macro

--- src/array/slice.rs
+++ src/array/slice.rs
@@ -1,21 +1,16 @@
-/// Returns tuple of start, step and end index
+/// Returns indices for slicing, each dimension separated with ';' and range with '=>'
 ///
 /// # Examples
 ///
 /// ```
-/// let example = s![2,4,5]; // Returns tuple of (2, 4, 5)
+/// let example = s![2,4; 5]; // Returns vector of [[2, 4], [5]]
 ///
-/// assert(s![1, 5], (1, 1, 5));
-/// assert(s![, 5], (0, 1, 5));
-/// assert(s![2, 4, 5], (2, 4, 5));
+/// assert(s![1; 5 => 2; 3], vec![vec![1], vec![5,2], vec![3]]);
+/// assert(s![1; 5; 2; 3], vec![vec![1], vec![5], vec![2], vec![3]]);
 /// ```
 #[macro_export]
 macro_rules! s {
-    ($start:expr, $step:expr, $end:expr) => (($start, $step, $end));
-    ($start:expr, $step:expr,) => (($start, $step, -1));
-    ($start:expr, $end:expr) => (($start, 1, $end));
-    (,$step:expr, $end:expr) => ((0, $step, $end));
-    ($start:expr) => (($start, 1, $start + 1));
-    ($start:expr,) => (($start, 1, -1));
-    (,$end:expr) => ((0, 1, $end));
+    ($($( $y:expr )=>*);*) => {
+        vec![ $(vec![$( $y ),*]),* ]
+    }
 }
