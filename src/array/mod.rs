226767559720c599893b6670355aeae518f4ee77
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-09 23:01:39 +0200

    Implement unit array macro

--- src/array/mod.rs
+++ src/array/mod.rs
@@ -2,3 +2,12 @@ mod array;
 mod slice;
 
 pub use self::array::Array;
+
+/// Creates unit array (array with only one element)
+#[macro_export]
+macro_rules! u {
+    ($y:expr) => {
+        Array::new(vec![$y], vec![1])
+    }
+}
+
