73d04515059c2402673af943753ac99674de3a6d
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-09 23:11:49 +0200

    Implement sub operator

--- src/operator/arithmetic.rs
+++ src/operator/arithmetic.rs
@@ -1,4 +1,5 @@
 use array::Array;
+use shape::Shape;
 
 use std::ops::{
     Add,
@@ -12,60 +13,90 @@ use std::ops::{
     MulAssign,
 };
 
+
+#[inline]
+fn check_shapes_compatibility(first: &Shape, second: &Shape) -> () {
+    if first != second {
+        panic!("Shapes are not compatible");
+    }
+}
+
 impl<T> Add for Array<T> where T: Clone + Add<Output=T> {
     type Output = Array<T>;
 
     fn add(self, other: Array<T>) -> Array<T> {
-        if self.shape == other.shape {
-            let first_data = self.data.borrow();
-            let second_data = other.data.borrow();
+        check_shapes_compatibility(&self.shape, &other.shape);
 
-            let mut data: Vec<T> = Vec::with_capacity(self.len());
+        let first_data = self.data.borrow();
+        let second_data = other.data.borrow();
 
-            for (f, s) in self.shape.get_bounds().zip(other.shape.get_bounds()) {
-                data.push(first_data[f].clone() + second_data[s].clone());
-            }
+        let mut data: Vec<T> = Vec::with_capacity(self.len());
 
-            return Array::new(data, self.get_shape().clone());
+        for (f, s) in self.shape.get_bounds().zip(other.shape.get_bounds()) {
+            data.push(first_data[f].clone() + second_data[s].clone());
         }
 
-        panic!("Shapes are not compatible");
+        return Array::new(data, self.get_shape().clone());
     }
 }
 
 impl<T> AddAssign for Array<T> where T: Clone + Add<Output=T> {
     fn add_assign(&mut self, other: Array<T>) -> () {
-        if self.shape == other.shape {
-            if self.data.as_ptr() == other.data.as_ptr() {
-                let mut first_data = self.data.borrow_mut();
-                for f in self.shape.get_bounds() {
-                    first_data[f] = first_data[f].clone() + first_data[f].clone();
-                }
-            } else {
-                let mut first_data = self.data.borrow_mut();
-                let second_data = other.data.borrow();
+        check_shapes_compatibility(&self.shape, &other.shape);
 
-                for (f, s) in self.shape.get_bounds().zip(other.shape.get_bounds()) {
-                    first_data[f] = first_data[f].clone() + second_data[s].clone();
-                }
+        if self.data.as_ptr() == other.data.as_ptr() {
+            let mut first_data = self.data.borrow_mut();
+            for f in self.shape.get_bounds() {
+                first_data[f] = first_data[f].clone() + first_data[f].clone();
             }
-            return;
-        }
+        } else {
+            let mut first_data = self.data.borrow_mut();
+            let second_data = other.data.borrow();
 
-        panic!("Shapes are not compatible");
+            for (f, s) in self.shape.get_bounds().zip(other.shape.get_bounds()) {
+                first_data[f] = first_data[f].clone() + second_data[s].clone();
+            }
+        }
     }
 }
 
-impl<T: Clone> Sub for Array<T> {
+impl<T> Sub for Array<T> where T: Clone + Sub<Output=T> {
     type Output = Array<T>;
 
     fn sub(self, other: Array<T>) -> Array<T> {
-        return self;
+        check_shapes_compatibility(&self.shape, &other.shape);
+
+        let first_data = self.data.borrow();
+        let second_data = other.data.borrow();
+
+        let mut data: Vec<T> = Vec::with_capacity(self.len());
+
+        for (f, s) in self.shape.get_bounds().zip(other.shape.get_bounds()) {
+            data.push(first_data[f].clone() - second_data[s].clone());
+        }
+
+        return Array::new(data, self.get_shape().clone());
     }
 }
 
-impl<T: Clone> SubAssign for Array<T> {
-    fn sub_assign(&mut self, other: Array<T>) -> () {}
+impl<T> SubAssign for Array<T> where T: Clone + Sub<Output=T> {
+    fn sub_assign(&mut self, other: Array<T>) -> () {
+        check_shapes_compatibility(&self.shape, &other.shape);
+
+        if self.data.as_ptr() == other.data.as_ptr() {
+            let mut first_data = self.data.borrow_mut();
+            for f in self.shape.get_bounds() {
+                first_data[f] = first_data[f].clone() - first_data[f].clone();
+            }
+        } else {
+            let mut first_data = self.data.borrow_mut();
+            let second_data = other.data.borrow();
+
+            for (f, s) in self.shape.get_bounds().zip(other.shape.get_bounds()) {
+                first_data[f] = first_data[f].clone() - second_data[s].clone();
+            }
+        }
+    }
 }
 
 impl<T: Clone> Mul for Array<T> {
