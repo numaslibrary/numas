cd319883d5f5955478762684d291d5b32c7f24ae
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-11 21:05:14 +0200

    Add logspace

--- src/factory/numeric.rs
+++ src/factory/numeric.rs
@@ -42,3 +42,26 @@ pub fn arange(start: f64, stop: f64, step: f64) -> Array<f64> {
     return Array::new(data, vec![num]);
 }
 
+/// Returns evenly spaced elements on log space from given interval
+///
+/// # Arguments
+///
+/// * `start` - start of interval
+/// * `stop` - end of interval
+/// * `base` - base of log
+/// * `num` - number of elements
+pub fn logspace(start: f64, stop: f64, base: f64, num: usize) -> Array<f64> {
+    let mut data: Vec<f64> = Vec::with_capacity(num);
+
+    let step: f64 = (stop - start) / (num - 1) as f64;
+    let mut tmp = start;
+
+    for _ in 0..num {
+        data.push(base.powf(tmp));
+        tmp += step;
+    }
+
+    return Array::new(data, vec![num as i32]);
+}
+
+
