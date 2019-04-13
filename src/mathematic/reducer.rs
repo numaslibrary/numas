156b4d446b20671a94b34aceba4e69a3576c0011
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-13 22:18:27 +0200

    Add reducers examples

--- src/mathematic/reducer.rs
+++ src/mathematic/reducer.rs
@@ -4,6 +4,16 @@ use array::Array;
 
 impl<T> Array<T> where T: Clone + From<u8> {
     /// Returns sums of all elements in array or view
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///  use numas::array::Array;;
+    ///
+    ///  let array = Array::new(vec![1, 2, 3, 4], vec![4]);
+    ///
+    ///  assert_eq!(array.sum(), 10);
+    /// ```
     pub fn sum(&self) -> T where T: AddAssign {
         let data = self.data.borrow();
         let mut accumulator = T::from(0 as u8);
@@ -16,6 +26,16 @@ impl<T> Array<T> where T: Clone + From<u8> {
     }
 
     /// Returns product of all elements in array or view
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///  use numas::array::Array;;
+    ///
+    ///  let array = Array::new(vec![1, 2, 3, 4], vec![4]);
+    ///
+    ///  assert_eq!(array.prod(), 24);
+    /// ```
     pub fn prod(&self) -> T where T: MulAssign {
         if self.len() == 0 {
             return T::from(0 as u8);
