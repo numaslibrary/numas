7df5443bf4a9dfbc378bb783dbd2a542dde01824
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-13 20:50:04 +0200

    Add tests to macros

--- src/array/mod.rs
+++ src/array/mod.rs
@@ -4,6 +4,19 @@ mod slice;
 pub use self::array::Array;
 
 /// Creates unit array (array with only one element)
+///
+/// # Examples
+///
+/// ```
+///  #[macro_use] extern crate numas;
+///  use numas::array::Array;
+///
+///  let array = u![8];
+///  let data = array.collect();
+///
+///  assert_eq!(data, vec![8]);
+///  assert_eq!(array.len(), 1);
+/// ```
 #[macro_export]
 macro_rules! u {
     ($y:expr) => {
--- src/array/slice.rs
+++ src/array/slice.rs
@@ -3,10 +3,12 @@
 /// # Examples
 ///
 /// ```
+/// #[macro_use] extern crate numas;
+///
 /// let example = s![2 => 4; 5]; // Returns vector of [[2, 4], [5]]
 ///
-/// assert(s![1; 5 => 2; 3], vec![vec![1], vec![5,2], vec![3]]);
-/// assert(s![1; 5; 2; 3], vec![vec![1], vec![5], vec![2], vec![3]]);
+/// assert_eq!(s![1; 5 => 2; 3], vec![vec![1], vec![5,2], vec![3]]);
+/// assert_eq!(s![1; 5; 2; 3], vec![vec![1], vec![5], vec![2], vec![3]]);
 /// ```
 #[macro_export]
 macro_rules! s {
