8bbc7a3bceecc0d1021e6f30fad5edf5bf7e4b0d
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-13 22:26:45 +0200

    Add docs to relational operators

--- src/operator/relational.rs
+++ src/operator/relational.rs
@@ -4,6 +4,24 @@ use std::cmp::PartialOrd;
 
 
 impl<T> Array<T> where T: Clone + PartialOrd {
+    /// Returns array of 1s and 0s representing truth value of lesser than element wise
+    ///
+    /// # Arguments
+    ///
+    /// * `other` - Array to compare with
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///  use numas::array::Array;;
+    ///
+    ///  let first = Array::new(vec![1, 2, 3, 3], vec![4]);
+    ///  let second = Array::new(vec![1, 0, 3, 4], vec![4]);
+    ///
+    ///  let less = first.lt(&second);
+    ///
+    ///  assert_eq!(less.collect(), vec![0, 0, 0, 1]);
+    /// ```
     pub fn lt(&self, other: &Array<T>) -> Array<u8> {
         super::check_shapes_compatibility(self.shape(), other.shape());
 
@@ -27,6 +45,24 @@ impl<T> Array<T> where T: Clone + PartialOrd {
         return Array::new(data, self.get_shape().clone());
     }
 
+    /// Returns array of 1s and 0s representing truth value of lesser equal element wise
+    ///
+    /// # Arguments
+    ///
+    /// * `other` - Array to compare with
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///  use numas::array::Array;;
+    ///
+    ///  let first = Array::new(vec![1, 2, 3, 3], vec![4]);
+    ///  let second = Array::new(vec![1, 0, 3, 4], vec![4]);
+    ///
+    ///  let less_equal = first.le(&second);
+    ///
+    ///  assert_eq!(less_equal.collect(), vec![1, 0, 1, 1]);
+    /// ```
     pub fn le(&self, other: &Array<T>) -> Array<u8> {
         super::check_shapes_compatibility(self.shape(), other.shape());
 
@@ -50,6 +86,24 @@ impl<T> Array<T> where T: Clone + PartialOrd {
         return Array::new(data, self.get_shape().clone());
     }
 
+    /// Returns array of 1s and 0s representing truth value of greater than element wise
+    ///
+    /// # Arguments
+    ///
+    /// * `other` - Array to compare with with
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///  use numas::array::Array;;
+    ///
+    ///  let first = Array::new(vec![1, 2, 3, 3], vec![4]);
+    ///  let second = Array::new(vec![1, 0, 3, 4], vec![4]);
+    ///
+    ///  let greater = first.gt(&second);
+    ///
+    ///  assert_eq!(greater.collect(), vec![0, 1, 0, 0]);
+    /// ```
     pub fn gt(&self, other: &Array<T>) -> Array<u8> {
         super::check_shapes_compatibility(self.shape(), other.shape());
 
@@ -73,6 +127,24 @@ impl<T> Array<T> where T: Clone + PartialOrd {
         return Array::new(data, self.get_shape().clone());
     }
 
+    /// Returns array of 1s and 0s representing truth value of greater equal element wise
+    ///
+    /// # Arguments
+    ///
+    /// * `other` - Array to compare with
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///  use numas::array::Array;;
+    ///
+    ///  let first = Array::new(vec![1, 2, 3, 3], vec![4]);
+    ///  let second = Array::new(vec![1, 0, 3, 4], vec![4]);
+    ///
+    ///  let greater_equal = first.ge(&second);
+    ///
+    ///  assert_eq!(greater_equal.collect(), vec![1, 1, 1, 0]);
+    /// ```
     pub fn ge(&self, other: &Array<T>) -> Array<u8> {
         super::check_shapes_compatibility(self.shape(), other.shape());
 
@@ -98,6 +170,24 @@ impl<T> Array<T> where T: Clone + PartialOrd {
 }
 
 impl<T> Array<T> where T: Clone + PartialEq {
+    /// Returns array of 1s and 0s representing truth value of equality element wise
+    ///
+    /// # Arguments
+    ///
+    /// * `other` - Array to comapre with
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///  use numas::array::Array;;
+    ///
+    ///  let first = Array::new(vec![1, 2, 3, 4], vec![4]);
+    ///  let second = Array::new(vec![1, 0, 3, 4], vec![4]);
+    ///
+    ///  let equality = first.eq(&second);
+    ///
+    ///  assert_eq!(equality.collect(), vec![1, 0, 1, 1]);
+    /// ```
     pub fn eq(&self, other: &Array<T>) -> Array<u8> {
         super::check_shapes_compatibility(self.shape(), other.shape());
 
@@ -115,6 +205,47 @@ impl<T> Array<T> where T: Clone + PartialEq {
         } else {
             for (f, s) in self.shape.get_bounds().zip(other.shape.get_bounds()) {
                 data.push(if first_data[f] == second_data[s] { 1 } else { 0 });
+            }
+        }
+
+        return Array::new(data, self.get_shape().clone());
+    }
+
+    /// Returns array of 1s and 0s representing truth value of not equality element wise
+    ///
+    /// # Arguments
+    ///
+    /// * `other` - Array to compare with
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///  use numas::array::Array;;
+    ///
+    ///  let first = Array::new(vec![1, 2, 3, 4], vec![4]);
+    ///  let second = Array::new(vec![1, 0, 3, 4], vec![4]);
+    ///
+    ///  let not_equality = first.neq(&second);
+    ///
+    ///  assert_eq!(not_equality.collect(), vec![0, 1, 0, 0]);
+    /// ```
+    pub fn neq(&self, other: &Array<T>) -> Array<u8> {
+        super::check_shapes_compatibility(self.shape(), other.shape());
+
+        let first_data = self.data.borrow();
+        let second_data = other.data.borrow();
+
+        let mut data: Vec<u8> = Vec::with_capacity(self.len());
+
+        if other.shape.total_len() == 1 {
+            let value = second_data[other.shape.get_bounds().start].clone();
+
+            for f in self.shape.get_bounds() {
+                data.push(if first_data[f] != value { 1 } else { 0 });
+            }
+        } else {
+            for (f, s) in self.shape.get_bounds().zip(other.shape.get_bounds()) {
+                data.push(if first_data[f] != second_data[s] { 1 } else { 0 });
             }
         }
 
