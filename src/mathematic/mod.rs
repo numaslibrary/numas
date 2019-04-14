f7281200c4f8b5664dfc79858d22f0fc609492a4
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-14 23:53:31 +0200

    Fix data bounding

--- src/mathematic/mod.rs
+++ src/mathematic/mod.rs
@@ -10,7 +10,9 @@ use array::Array;
 fn apply<T, S, R>(array: &Array<T>, function: S ) -> Array<R>
     where T: Clone, R: Clone, S: FnMut(&T) -> R,
 {
-        let data = array.data.borrow().iter().map(function).collect();
+        let old_data = array.data.borrow();
+        let mut data: Vec<R> = old_data[array.shape().get_bounds()].iter().map(function).collect();
+
         return Array::new(data, array.get_shape().clone());
 }
 
